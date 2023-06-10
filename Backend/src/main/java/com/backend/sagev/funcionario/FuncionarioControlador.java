7bb5a72e07e6677130815774235146eea39347f3
Emanuel Pérez <ema002ps@gmail.com>
2023-06-09 23:05:22 -0600

    rfix: validaciones en botones de cancelar-cita-tabla

--- Backend/src/main/java/com/backend/sagev/funcionario/FuncionarioControlador.java
+++ Backend/src/main/java/com/backend/sagev/funcionario/FuncionarioControlador.java
@@ -12,7 +12,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = {"http://localhost:4200",
+                        "http://10.10.11.29:4200",
+                        "http://localhost:8055",
+                        "http://10.10.11.29:8055"}, 
+                        allowedHeaders = "*",maxAge = 3600)
 @RestController
 @RequestMapping({"/funcionarios"})
 public class FuncionarioControlador {
